5365a644bc26a79cb885bdc6e711fac36b615954
Believer000 <artem.ostapyuk@mail.ru>
2022-09-12 17:18:44 +0300

    Initial commit

---
+++ Assets/Scripts/CameraRotateAround.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class CameraRotateAround : MonoBehaviour {
+
+    public float sensitivity = 3.5f;
+
+    public float minAngleX = -40.0f;
+    public float maxAngleX = 30.0f;
+
+    private float _angleX = 0.0f;
+    private float _angleY = 0.0f;
+
+    private void Awake()
+    {
+        _angleX = transform.localEulerAngles.x;
+        _angleY = transform.localEulerAngles.y;
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButton(0))
+            return;
+
+        _angleX -= Input.GetAxis("Mouse Y") * sensitivity;
+        _angleY += Input.GetAxis("Mouse X") * sensitivity;
+
+        _angleX = Mathf.Clamp(_angleX, minAngleX, maxAngleX);
+
+        transform.localEulerAngles = new Vector3(_angleX, _angleY, transform.localEulerAngles.z);
+    }
+}
